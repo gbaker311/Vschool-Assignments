09d716702842af15566afa93b9a757362da625d4
greg <sweetwinemusic@gmail.com>
2020-11-15 03:57:35 -0700

    first commit

--- fizzBuzz/app.js
+++ fizzBuzz/app.js
@@ -3,6 +3,7 @@
 //  For each multiple of 5, print "Buzz" instead of the number.
 //  For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
+
 for (let i = 0; i <= 100; i++) {
   console.log(i);
   if (i % 5 === 0 && i % 3 === 0) {
