0ceb7ebe21b42117b916dee603d1cb10a0d36725
greg <sweetwinemusic@gmail.com>
2020-11-15 02:46:00 -0700

    finished for loop

--- fizzBuzz/app.js
+++ fizzBuzz/app.js
@@ -4,14 +4,14 @@
 // For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
 for (let i = 0; i <= 100; i++) {
-    console.log(i)
-    if (i % 5 === 0 && i % 3 === 0) {
-        console.log("fizz-buzz");
-    }   else if (i % 3 === 0) {
-        console.log("fizz");
-    }   else if (i % 5 === 0) {
-        console.log("buzz")
-    }   else {
-        console.log(i)
-    }
+  console.log(i);
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("fizz-buzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
 }
