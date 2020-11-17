c5d3fb2954d5e5ad66349890ab4654b322e82591
greg <sweetwinemusic@gmail.com>
2020-11-17 13:56:07 -0700

    removed git from sub directory

--- RPG/app.js
+++ RPG/app.js
@@ -1,27 +1,105 @@
-const readLineSync = require("readline-sync");
+const readline = require('readline-sync');
+
+
+function startGame() {
+    const steps = {
+      start: {
+        message: "Do you want to play a game?",
+        yes: "firstStep"
+      },
+      end: {
+        message: "You've lost either way. Do you want to play again?",
+        yes: "start",
+        no: () => {
+          console.clear();
+        }
+      },
+      firstStep: {
+        message: "Do you agree?"
+      }
+      // put your steps here
+    };
   
-const Character = require("./Character");
-const commands = ["w", "walk", "u", "use", "p", "print", "q", "quit", "h", "help"];
-
-
-class Character {
-    constructor()
-}
-
-
-
-class Truck {
-    constructor(make, model, year, airHorn) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.airHorn = airHorn;
+    let currentStep = "start";
+  
+    function logStep() {
+      const step = steps[currentStep];
+  
+      if (step) {
+        console.log(`${step.message || ""} Y/N`);
+      }
     }
-}
+    function handleAnswer(agree) {
+      let step;
+  
+      if (agree) {
+        step = steps[currentStep].yes;
+      } else {
+        step = steps[currentStep].no;
+      }
+  
+      if (typeof step === "function") {
+        step();
+        return;
+      }
+      
+      if (typeof step === "string") {
+        currentStep = step;
+      } else {
+        currentStep = "end";
+      }
+      
+      logStep();
+    }
+  
+    document.addEventListener("keydown", args => {
+      const key = args.key.toLowerCase();
+  
+      if (key === "y") {
+        handleAnswer(true);
+      } else if (key === "n") {
+        handleAnswer(false);
+      }
+    });
+  
+    console.clear();
+    logStep();
+  }
+  
+  startGame();
 
-const semiTruck = new Truck("Freitliner", "Cascadia", 2020, "Bluuuuuuuurp")
 
-Truck.prototype.airHorn = function() {
-    console.log("Bluuuuuuuurp!!!")
-}
-console.log(semiTruck.airHorn)
+// const commands = [
+//   "w",
+//   "walk",
+//   "u",
+//   "use",
+//   "p",
+//   "print",
+//   "q",
+//   "quit",
+//   "h",
+//   "help"
+// ];
+
+// class Character {
+//     constructor(name, job, health, status) {
+
+//     }
+// }
+
+// class Truck {
+//     constructor(make, model, year, airHorn) {
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//         this.airHorn = airHorn;
+//     }
+// }
+
+// const semiTruck = new Truck("Freitliner", "Cascadia", 2020, "Bluuuuuuuurp")
+
+// Truck.prototype.airHorn = function() {
+//     console.log("Bluuuuuuuurp!!!")
+// }
+// console.log(semiTruck.airHorn)
